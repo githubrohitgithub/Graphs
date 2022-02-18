cc0289c41f33e1deeff2ea3ed6d0144a844b644a
CC4 <rpachori21@gmail.com>
2022-02-18 15:53:16 +0530

    range chart

--- app/src/main/java/com/example/scatteredgraph/MainActivity.kt
+++ app/src/main/java/com/example/scatteredgraph/MainActivity.kt
@@ -3,10 +3,8 @@ package com.example.scatteredgraph
 import android.content.Intent
 import android.graphics.Color
 import android.os.Bundle
-import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 import com.example.scatteredgraph.databinding.ActivityMainBinding
-import com.github.mikephil.charting.charts.BarChart
 import com.github.mikephil.charting.charts.ScatterChart
 import com.github.mikephil.charting.components.LimitLine
 import com.github.mikephil.charting.components.XAxis
@@ -19,27 +17,19 @@ class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
 
-    private lateinit var barData: BarData
-    lateinit var barDataSet: BarDataSet
-    lateinit var barEntries: ArrayList<BarEntry>
-    private var scatterEntries1: ArrayList<Entry>?=null
-    private var scatterEntries2: ArrayList<Entry>?=null
-    private var scatterEntries3: ArrayList<Entry>?=null
+    private val dataSets by lazy { ArrayList<IScatterDataSet>() }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        binding.candle.setOnClickListener {
+        binding.btnCandle.setOnClickListener {
             startActivity(Intent(applicationContext,MainActivity2::class.java))
         }
 
-        entries1
-        entries2
-        entries3
 
-        val set1 = ScatterDataSet(scatterEntries1, "Data set 1")
+        val set1 = ScatterDataSet(initDataset1(), "Data set 1")
         set1.apply {
             this.setScatterShape(ScatterChart.ScatterShape.CIRCLE)
             this.color = Color.BLUE
@@ -47,7 +37,7 @@ class MainActivity : AppCompatActivity() {
             this.scatterShapeSize = 15f
         }
 
-        val set2 = ScatterDataSet(scatterEntries2, "Data set 2")
+        val set2 = ScatterDataSet(initDataset2(), "Data set 2")
         set2.apply {
             this.setScatterShape(ScatterChart.ScatterShape.CIRCLE)
             this.color = Color.GREEN
@@ -55,8 +45,7 @@ class MainActivity : AppCompatActivity() {
             this.scatterShapeSize = 15f
         }
 
-
-        val set3 = ScatterDataSet(scatterEntries3, "Data set 3")
+        val set3 = ScatterDataSet(initDataset3(), "Data set 3")
         set3.apply {
             this.setScatterShape(ScatterChart.ScatterShape.CIRCLE)
             this.color = Color.RED
@@ -64,12 +53,9 @@ class MainActivity : AppCompatActivity() {
             this.scatterShapeSize = 15f
         }
 
-
-        val dataSets: ArrayList<IScatterDataSet> = ArrayList()
         dataSets.add(set1)
         dataSets.add(set2)
         dataSets.add(set3)
-
 
         binding.scatterChart.data = ScatterData(dataSets)
         binding.scatterChart.invalidate()
@@ -89,83 +75,51 @@ class MainActivity : AppCompatActivity() {
 
         }
 
+    }
 
-      barchatInit()
+    private fun initDataset1():ArrayList<Entry> {
+
+        val scatterEntries by lazy { ArrayList<Entry>() }
+        scatterEntries.add(BarEntry(1f, 10F))
+        scatterEntries.add(BarEntry(2f, 10F))
+        scatterEntries.add(BarEntry(3f, 10F))
+        scatterEntries.add(BarEntry(4f, 10F))
+        scatterEntries.add(BarEntry(5f, 10F))
+        scatterEntries.add(BarEntry(6f, 10F))
+        scatterEntries.add(BarEntry(7f, 10F))
+
+        return scatterEntries
 
     }
 
-    private fun barchatInit() {
-        getBarEntries()
-        barDataSet = BarDataSet(barEntries, "")
-        barDataSet.setDrawValues(false)
-        barData = BarData(barDataSet)
-        binding.barChart.data = barData
-        barDataSet.color = Color.BLUE
-        barDataSet.valueTextColor = Color.BLACK
-        barDataSet.valueTextSize = 18f
+    private fun initDataset2():ArrayList<Entry> {
 
-        val xAxisLabelsbar = arrayListOf("","Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun")
-        binding.barChart.apply {
-            this.xAxis.position = XAxis.XAxisPosition.BOTTOM
-            this.xAxis.textSize = 12f
-            this.axisRight.isEnabled = false
-            this.axisLeft.setDrawGridLines(true)
-            this.xAxis.setDrawGridLines(false)
-            this.legend.isEnabled = false
-            this.description.isEnabled = false
-            this.xAxis.valueFormatter = IndexAxisValueFormatter(xAxisLabelsbar)
+        val scatterEntries by lazy { ArrayList<Entry>() }
+        scatterEntries.add(BarEntry(1f, 120F))
+        scatterEntries.add(BarEntry(2f, 114F))
+        scatterEntries.add(BarEntry(3f, 118F))
+        scatterEntries.add(BarEntry(4f, 120F))
+        scatterEntries.add(BarEntry(5f, 160F))
+        scatterEntries.add(BarEntry(6f, 175F))
+        scatterEntries.add(BarEntry(7f, 123F))
 
-        }
+        return scatterEntries
     }
 
 
-    private fun getBarEntries() {
-        barEntries = ArrayList()
-        barEntries.add(BarEntry(1F, 70F))
-        barEntries.add(BarEntry(2F, 90F))
-        barEntries.add(BarEntry(3F, 11F))
-        barEntries.add(BarEntry(4F, 120F))
-        barEntries.add(BarEntry(5F, 130F))
-        barEntries.add(BarEntry(6F, 150F))
+    private fun initDataset3():ArrayList<Entry> {
+
+        val scatterEntries by lazy { ArrayList<Entry>() }
+        scatterEntries.add(BarEntry(1f, 200F))
+        scatterEntries.add(BarEntry(2f, 210F))
+        scatterEntries.add(BarEntry(3f, 210F))
+        scatterEntries.add(BarEntry(4f, 230F))
+        scatterEntries.add(BarEntry(5f, 240F))
+        scatterEntries.add(BarEntry(6f, 10F))
+        scatterEntries.add(BarEntry(7f, 10F))
+
+        return scatterEntries
     }
 
-    private val entries1: Unit
-        get() {
-            scatterEntries1 = ArrayList()
-            scatterEntries1?.add(BarEntry(1f, 10F))
-            scatterEntries1?.add(BarEntry(2f, 10F))
-            scatterEntries1?.add(BarEntry(3f, 10F))
-            scatterEntries1?.add(BarEntry(4f, 10F))
-            scatterEntries1?.add(BarEntry(5f, 10F))
-            scatterEntries1?.add(BarEntry(6f, 10F))
-            scatterEntries1?.add(BarEntry(7f, 10F))
 
-        }
-
-
-    private val entries2: Unit
-        get() {
-            scatterEntries2 = ArrayList()
-            scatterEntries2?.add(BarEntry(1f, 120F))
-            scatterEntries2?.add(BarEntry(2f, 114F))
-            scatterEntries2?.add(BarEntry(3f, 118F))
-            scatterEntries2?.add(BarEntry(4f, 120F))
-            scatterEntries2?.add(BarEntry(5f, 160F))
-            scatterEntries2?.add(BarEntry(6f, 175F))
-            scatterEntries2?.add(BarEntry(7f, 123F))
-
-        }
-
-    private val entries3: Unit
-        get() {
-            scatterEntries3 = ArrayList()
-            scatterEntries3?.add(BarEntry(1f, 200F))
-            scatterEntries3?.add(BarEntry(2f, 210F))
-            scatterEntries3?.add(BarEntry(3f, 210F))
-            scatterEntries3?.add(BarEntry(4f, 230F))
-            scatterEntries3?.add(BarEntry(5f, 240F))
-            scatterEntries3?.add(BarEntry(6f, 10F))
-            scatterEntries3?.add(BarEntry(7f, 10F))
-
-        }
 }
--- app/src/main/java/com/example/scatteredgraph/MainActivity2.kt
+++ app/src/main/java/com/example/scatteredgraph/MainActivity2.kt
@@ -3,11 +3,10 @@ package com.example.scatteredgraph
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.anychart.AnyChart
-import com.anychart.AnyChartView
 import com.anychart.chart.common.dataentry.DataEntry
+import com.anychart.core.ui.ChartCredits
 import com.anychart.data.Set
 import com.example.scatteredgraph.databinding.ActivityMain2Binding
-import com.example.scatteredgraph.databinding.ActivityMainBinding
 
 
 class MainActivity2 : AppCompatActivity() {
@@ -18,6 +17,8 @@ class MainActivity2 : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMain2Binding.inflate(layoutInflater)
         setContentView(binding.root)
+
+
         binding.anyChartView.setProgressBar(findViewById(R.id.progress_bar))
         val cartesian = AnyChart.cartesian()
 
@@ -31,21 +32,28 @@ class MainActivity2 : AppCompatActivity() {
             this.legend(true)
             this.yGrid(true)
                 .yMinorGrid(true)
-            this.tooltip().titleFormat("{%SeriesName} ({%x})")
+
         }
 
         val set = Set.instantiate()
         set.data(dataList())
-        val columnLondon = cartesian.rangeColumn(set.mapAs("{ x: 'x', high: 'pressureHigh', low: 'pressureLow' }"))
-        columnLondon.name("Blood Pressure")
+
+        val columnBar = cartesian.rangeColumn(set.mapAs("{ x: 'x', high: 'pressureHigh', low: 'pressureLow' }"))
+
+        columnBar.apply {
+            name("Blood Pressure")
+            color("#1c64f2")
+        }
+
         binding.anyChartView.setChart(cartesian)
+
     }
 
    private fun dataList():MutableList<DataEntry> {
        val data: MutableList<DataEntry> = ArrayList()
        data.add(CustomDataEntry("Mon",  90.1, 120.9))
        data.add(CustomDataEntry("Tue",  100.5, 130.2))
-       data.add(CustomDataEntry("Wed", 75.9, 125.1))
+       data.add(CustomDataEntry("Wed",  75.9, 125.1))
        data.add(CustomDataEntry("Thu",  700.1, 120.8))
        data.add(CustomDataEntry("Fri",  111.3, 125.7))
        data.add(CustomDataEntry("Sat",  90.7, 130.5))
